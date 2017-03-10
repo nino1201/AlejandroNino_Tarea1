788ad0628f7f42d8c89d8e294d67fcc490293d74
ggjuancamilo <jc.gallego10@uniandes.edu.co>
2017-03-10 10:45:13 -0500

    graficas modificadas

--- grafica.py
+++ grafica.py
@@ -1,13 +1,17 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-E=np.genfromtxt("a.dat")
-X=np.linspace(0,999,1000)
+E=np.genfromtxt("datos.dat")
+X=np.linspace(0,1000,len(E[:1000,0]))
 
 plt.figure()
-plt.plot(X,E[:,0])
-plt.plto(X,E[:,1])
-plt.plot(X,E[:,2])
+plt.plot(X,E[:1000,0], label="Modo 1")
+plt.plot(X,E[:1000,1], label="Modo 2")
+plt.plot(X,E[:1000,2], label="Modo 3")
+plt.legend()
+plt.title("Energias")
+plt.ylabel("E")
+plt.xlabel("Tiempo")
 plt.savefig("energias.pdf")
 plt.show()
 plt.close()
--- tiempos.py
+++ tiempos.py
@@ -2,5 +2,5 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 datos=np.genfromtxt('tiempos.dat')
-plt.scatter(datos[:,0],datos[:,1])
+plt.scatter(datos[:,1],datos[:,0])
 plt.savefig("tiempos.pdf")
